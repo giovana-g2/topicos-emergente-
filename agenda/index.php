f80de8c6b6c9a1dc9855365901b87ebd21daca15
Giovana Gamarra <giovana.2023302580@aluno.iffar.edu.br>
2025-05-08 15:32:26 -0300

    Update index.php

--- agenda/index.php
+++ agenda/index.php
@@ -19,5 +19,20 @@
          <input type="submit" value=Cadastrar>
 
          </form>
+        <?php
+include ("conexao.php"); 
+
+$sql = "SELECT * FROM contatos"; //procurar e mostrar o que tem em contatos *
+
+$res = mysqli_query($conexao, $sql); //executando
+
+if (mysqli_num_rows($res) > 0) {
+    while ($linha = mysqli_fetch_assoc($res)) { 
+        echo $linha['nome'] . " - " . $linha['endereco'] . " - " . $linha['telefone'] .  "<br>";
+    }
+} else {
+    echo "Nenhum contato encontrado.";
+}
+?>
 </body>
 </html>
